87b16e0508393d5873ca81d63b4ba722dae1208a
kazewong <kazewong.physics@gmail.com>
2022-12-07 01:56:09 -0500

    Switch to MALA since it is better tested

--- example/ParameterEstimation/ripple_HMC.py
+++ example/ParameterEstimation/ripple_HMC.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 # from functools import partial
 
 # Import FlowMC stuff
-from flowMC.sampler.HMC import HMC
+# from flowMC.sampler.HMC import HMC
+from flowMC.sampler.MALA import MALA
 from flowMC.nfmodel.rqSpline import RQSpline
 from flowMC.sampler.Sampler import Sampler
 from flowMC.utils.PRNG_keys import initialize_rng_keys
@@ -55,8 +56,8 @@ gmst = GreenwichMeanSiderealTime(trigger_time)
 # True parameters for injection
 
 # Mc, eta, chi1, chi2, dist, tc, phic, inclination, polarization_angle, ra, dec,
-m1 = 60
-m2 = 50
+m1 = 30
+m2 = 25
 Mc, eta = ms_to_Mc_eta(jnp.array([m1, m2]))
 true_params = jnp.array(
     [Mc, eta, 0.3, -0.4, 800, 0.0, 0.0, np.pi / 3, np.pi / 3, np.pi / 3, np.pi / 3]
@@ -84,10 +85,9 @@ L1_data = L1_noise_psd + L1_signal
 def logL(p):
     # Adding on the true ones
     extrinsic_variables = jnp.array(
-        [800, 0.0, 0.0, np.pi / 3, np.pi / 3, np.pi / 3, np.pi / 3]
+        [1600, 0.0, 0.0, np.pi / 3, np.pi / 3, np.pi / 3, np.pi / 3]
     )
     params = jnp.concatenate((p, extrinsic_variables))
-    print(params)
     logL_H1 = single_detector_likelihood(
         waveform_generator, params, H1_data, f_list, psd_list[0], H1, gmst, epoch
     )
@@ -97,49 +97,69 @@ def logL(p):
 
     return logL_H1 + logL_L1
 
+prior_range = jnp.array(
+    [
+        [10, 80],
+        [0.09, 0.25],
+        [0, 1],
+        [0, 1]
+    ]
+)
 
-# fake_params = jnp.array([Mc, eta, 0.3, -0.4])
-# print(logL(fake_params))
-# quit()
+def top_hat(x):
+    output = 0.0
+    for i in range(n_dim):
+        output = jax.lax.cond(
+            x[i] >= prior_range[i, 0], lambda: output, lambda: -jnp.inf
+        )
+        output = jax.lax.cond(
+            x[i] <= prior_range[i, 1], lambda: output, lambda: -jnp.inf
+        )
+    return output
 
+def posterior(theta):
+    prior = top_hat(theta)
+    return logL(theta) + prior
 
-#########################################
-# Purely HMC below
-#########################################
-
-
-# def dual_moon_pe(x):
-#     """
-#     Term 2 and 3 separate the distribution and smear it along the first and second dimension
-#     """
-#     term1 = 0.5 * ((jnp.linalg.norm(x) - 2) / 0.1) ** 2
-#     term2 = -0.5 * ((x[:1] + jnp.array([-3.0, 3.0])) / 0.8) ** 2
-#     term3 = -0.5 * ((x[1:2] + jnp.array([-3.0, 3.0])) / 0.6) ** 2
-#     return -(term1 - logsumexp(term2) - logsumexp(term3))
 
 
 n_dim = 4
-n_chains = 15
-n_local_steps = 3
+n_chains = 5
+n_local_steps = 30000
 n_global_steps = 30
-step_size = 0.1
+step_size = 1
 n_loop_training = 2
-n_loop_production = 2
-n_leapfrog = 10
+n_loop_production = 1
+n_leapfrog = 5
 
-HMC = HMC(
-    logL,
-    True,
-    {
-        "step_size": step_size,
-        "n_leapfrog": n_leapfrog,
-        "inverse_metric": jnp.ones(n_dim),
-    },
-)
+true_params = jnp.array([Mc, eta, 0.3, -0.4])
+rng_key_set = initialize_rng_keys(n_chains, seed=41)
 
-rng_key_set = initialize_rng_keys(n_chains, seed=42)
+initial_noise = jax.random.normal(rng_key_set[0], shape=(n_chains, n_dim)) * 1
+initial_mean = jnp.array([Mc, eta, 0.3, -0.4]).reshape(1,4)
+sigma = jnp.array([0.01, 0.001, 0.01, 0.01]).reshape(1,4)
 
-initial_position = jax.random.normal(rng_key_set[0], shape=(n_chains, n_dim)) * 1
+initial_position = initial_mean + sigma * initial_noise
+
+mass_diag = lambda x: jnp.abs(1./(jax.grad(logL)(x)+jax.grad(top_hat)(x)))
+
+mass_matrix = np.eye(n_dim)
+mass_matrix = jax.vmap(mass_diag)(initial_position)
+mass_matrix = jnp.array(mass_matrix).mean(axis=0)
+
+
+# HMC_init = HMC(
+#     logL,
+#     True,
+#     {
+#         "step_size": step_size,
+#         "n_leapfrog": n_leapfrog,
+#         "inverse_metric": mass_matrix,
+#     },
+# )
+
+MALA_init = MALA(logL, True, {"step_size": step_size*mass_matrix*jnp.eye(n_dim)})
+
 
 model = RQSpline(n_dim, 4, [32, 32], 8)
 
@@ -148,8 +168,8 @@ print("Initializing sampler class")
 nf_sampler = Sampler(
     n_dim,
     rng_key_set,
-    HMC,
-    logL,
+    MALA_init,
+    posterior,
     model,
     n_loop_training=n_loop_training,
     n_loop_production=n_loop_production,
@@ -160,3 +180,5 @@ nf_sampler = Sampler(
 )
 
 nf_sampler.sample(initial_position)
+
+chains, log_prob, local_accs, global_accs = nf_sampler.get_sampler_state().values()
