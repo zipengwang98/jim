6cdf6476236b69425177c5c171db7c968f4a7ec0
Kaze Wong <kazewong.physics@gmail.com>
2023-06-30 15:50:27 -0400

    Update GW150914. The original prior for iota and dec might resulted in nan, which kills some of the chain. Now fixed

--- example/ParameterEstimation/GW150914.py
+++ example/ParameterEstimation/GW150914.py
@@ -165,15 +165,15 @@ logL = make_heterodyne_likelihood_mutliple_detector(data_list, psd_list, respons
 ###########################################
 
 n_dim = 11
-n_chains = 1000
-n_loop_training = 20
+n_chains = 500
+n_loop_training = 10
 n_loop_production = 10
 n_local_steps = 200
 n_global_steps = 200
 learning_rate = 0.001
 max_samples = 100000
 momentum = 0.9
-num_epochs = 60
+num_epochs = 200
 batch_size = 50000
 
 guess_param = best_fit
