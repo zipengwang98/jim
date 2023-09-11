434f05a45bb0359389065064a88a7d5e756a01e8
zipengwang98 <scientistwang9877@gmail.com>
2023-09-11 15:28:54 -0400

    Pv2 PPE tested

--- example/corner_plot.py
+++ example/corner_plot.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import corner
 from scipy.interpolate import interp1d
 
-n_dim = 12
+n_dim = 16
 
-data = np.load("./PPE_GW150914_14.npz")
+data = np.load("./PPE_GW150914_9_pv2.npz")
 chains = data["chains"]
 log_prob = data["log_prob"]
 mask = (log_prob >-5000.)
@@ -27,29 +27,28 @@ print(chains.shape)
 #         log_prob_raw[i] = 0
 # log_prob = log_prob = np.array([log_prob_raw]).T
 
-#s1_theta = chains[:,2]
-#s1_phi = chains[:,3]
-#s1_mag = chains[:,4]
-#s2_theta = chains[:,5]
-#s2_phi = chains[:,6]
-#s2_mag = chains[:,7]
-#
-#
-#
-## s1x, s1y, and s1z
-#chains[:, 2] = s1_mag * np.sin(s1_theta) * np.cos(s1_phi)
-#chains[:, 3] = s1_mag * np.sin(s1_theta) * np.sin(s1_phi)
-#chains[:, 4] = s1_mag * np.cos(s1_theta) 
-#
-#
-## s2x, s2y, and s2z
-#chains[:, 5] = s2_mag * np.sin(s2_theta) * np.cos(s2_phi)
-#chains[:, 6] = s2_mag * np.sin(s2_theta) * np.sin(s2_phi)
-#chains[:, 7] = s2_mag * np.cos(s2_theta) 
-#
-#chains[:, 11] = np.arccos(np.arcsin(np.sin(chains[:, 11]/2*np.pi))*2/np.pi)
-#chains[:, 14] = np.arcsin(np.arcsin(np.sin(chains[:, 14]/2*np.pi))*2/np.pi)
+s1_theta = chains[:,2]
+s1_phi = chains[:,3]
+s1_mag = chains[:,4]
+s2_theta = chains[:,5]
+s2_phi = chains[:,6]
+s2_mag = chains[:,7]
 
+
+
+# s1x, s1y, and s1z
+chains[:, 2] = s1_mag * np.sin(s1_theta) * np.cos(s1_phi)
+chains[:, 3] = s1_mag * np.sin(s1_theta) * np.sin(s1_phi)
+chains[:, 4] = s1_mag * np.cos(s1_theta) 
+
+
+# s2x, s2y, and s2z
+chains[:, 5] = s2_mag * np.sin(s2_theta) * np.cos(s2_phi)
+chains[:, 6] = s2_mag * np.sin(s2_theta) * np.sin(s2_phi)
+chains[:, 7] = s2_mag * np.cos(s2_theta) 
+
+chains[:, 11] = np.arccos(np.arcsin(np.sin(chains[:, 11]/2*np.pi))*2/np.pi)
+chains[:, 14] = np.arcsin(np.arcsin(np.sin(chains[:, 14]/2*np.pi))*2/np.pi)
 #########################
 # chains = np.concatenate((chains, log_prob), axis=1)
 chains = chains[::50]
@@ -68,11 +67,11 @@ figure = corner.corner(
     labels=[
         "$M_c$",
         "$q$",
-        #"$s_1x$",
-        #"$s_1y$",
+        "$s_1x$",
+        "$s_1y$",
         "$s_1z$",
-        #"$s_2x$",
-        #"$s_2y$",
+        "$s_2x$",
+        "$s_2y$",
         "$s_2z$",
         "$D$",
         "$t_c$",
@@ -93,4 +92,4 @@ figure = corner.corner(
     show_titles=True,
 )
 #figure.savefig("./phenomp_corner.png")
-figure.savefig("./PPE_plots/PPE_14_phenomd_corner.png")
+figure.savefig("./PPE_plots/PPE_9_phenomp_corner.png")
--- src/jimgw/waveform.py
+++ src/jimgw/waveform.py
@@ -2,6 +2,7 @@ from jaxtyping import Array
 from ripple.waveforms.IMRPhenomD import gen_IMRPhenomD_hphc
 from ripple.waveforms.IMRPhenomPv2 import gen_IMRPhenomPv2_hphc
 from ripple.waveforms.PPE_IMRPhenomD import gen_PPE_IMRPhenomD_hphc
+from ripple.waveforms.PPE_IMRPhenomPv2 import gen_PPE_IMRPhenomPv2_hphc
 import jax.numpy as jnp
 from abc import ABC
 
@@ -93,9 +94,9 @@ class RippleIMRPhenomPv2(Waveform):
 
     f_ref: float
 
-    def __init__(self, f_ref: float = 20.0):
+    def __init__(self, f_ref: float = 20.0, ppe_index: int = 0):
         self.f_ref = f_ref
-
+        self.ppe_index = ppe_index
     def __call__(self, frequency: Array, params: dict) -> Array:
         output = {}
         theta = [
@@ -113,6 +114,39 @@ class RippleIMRPhenomPv2(Waveform):
             params["iota"],
         ]
         hp, hc = gen_IMRPhenomPv2_hphc(frequency, theta, self.f_ref)
+        output["p"] = hp
+        output["c"] = hc
+        return output
+
+
+class RipplePPEIMRPhenomPv2(Waveform):
+
+    f_ref: float
+
+    def __init__(self, f_ref: float = 20.0, ppe_index: int = 0):
+        self.f_ref = f_ref
+        self.ppe_index = ppe_index
+
+    def __call__(self, frequency: Array, params: dict) -> Array:
+        output = {}
+        theta = [
+            params["M_c"],
+            params["eta"],
+            params['s1_x'],
+            params['s1_y'],
+            params["s1_z"],
+            params['s2_x'],
+            params['s2_y'],
+            params["s2_z"],
+            params["d_L"],
+            0,
+            params["phase_c"],
+            params["iota"],
+        ]
+        ppe_index = self.ppe_index
+        ppes = jnp.zeros(15)
+        ppes = ppes.at[ppe_index].set(params["ppe"])
+        hp, hc = gen_PPE_IMRPhenomPv2_hphc(frequency, theta, ppes, self.f_ref)
         output["p"] = hp
         output["c"] = hc
         return output
